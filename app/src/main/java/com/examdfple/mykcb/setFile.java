c0e9eb7d4a812c2b2236e57b54de5f3e59db7c05
GSHAO <1462905973@qq.com>
2023-11-14 09:34:35 +0800

    侧滑栏列表 添加前

---
+++ app/src/main/java/com/examdfple/mykcb/setFile.java
@@ -0,0 +1,42 @@
+package com.examdfple.mykcb;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class setFile{
+    private final Context mContext;
+    setFile(Context mContext){
+        super();
+        this.mContext = mContext;
+    }
+    /** 文件名
+     * @param {*} 文件名
+     * */
+    public Boolean write(String fielname,String obj){
+        try{
+            FileOutputStream output = mContext.openFileOutput(fielname, Context.MODE_PRIVATE);
+            output.write(obj.getBytes());  //将String字符串以字节流的形式写入到输出流中
+            output.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+    public String read(String filename) throws IOException {
+        //打开文件输入流
+        FileInputStream input = mContext.openFileInput(filename);
+        byte[] temp = new byte[1024];
+        StringBuilder sb = new StringBuilder("");
+        int len = 0;
+        //读取文件内容:
+        while ((len = input.read(temp)) > 0) {
+            sb.append(new String(temp, 0, len));
+        }
+        //关闭输入流
+        input.close();
+        return sb.toString();
+    }
+}
